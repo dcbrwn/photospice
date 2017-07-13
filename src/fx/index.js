ffe9797d5bd5df32e11141d0824107b2dbc569e7
Danila Shutov <dcbrwn2@gmail.com>
2017-07-14 02:18:36 +0400

    Add grouping for fx

---
+++ src/fx/index.js
@@ -0,0 +1,11 @@
+import colorize from './colorize.js';
+import funky from './funky.js';
+import grayscale from './grayscale.js';
+import scaline from './scaline.js';
+
+export default {
+  colorize,
+  funky,
+  grayscale,
+  scaline,
+};
