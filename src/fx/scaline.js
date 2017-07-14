ef052cc5bcefc839caaeb5544e937e5c62b3bde4
Danila Shutov <dcbrwn2@gmail.com>
2017-07-14 18:22:10 +0400

    Fix incorrect height handling in scaline shader

--- src/fx/scaline.js
+++ src/fx/scaline.js
@@ -8,7 +8,7 @@ uniform vec2 uResolution;
 void main() {
   vec2 uv = gl_FragCoord.xy / uResolution.xy;
   vec4 src = texture2D(uImage, uv, 0.0);
-  gl_FragColor = src * clamp(abs(sin(uv.y * 500.0)), 0.7, 1.0) * (1.0 - 1.5 * length(uv - 0.5));
+  gl_FragColor = src * (mod(gl_FragCoord.y, 2.0) > 1.0 ? 0.7 : 1.0);
   gl_FragColor.a = src.a;
 }
 `;
