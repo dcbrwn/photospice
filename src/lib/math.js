ac2062232f730a001c0cbb77e5453914cbc8a490
Danila Shutov <dcbrwn2@gmail.com>
2017-07-24 19:25:10 +0400

    Fix incorrect RGB2HSV conversion

--- src/lib/math.js
+++ src/lib/math.js
@@ -15,7 +15,7 @@ export function align(value, step) {
   return step * Math.round(value / step);
 }
 
-export function HUEToRGB(H) {
+export function HueToRGB(H) {
   const R = clamp(Math.abs(H * 6.0 - 3.0) - 1.0, 0, 1);
   const G = clamp(2.0 - Math.abs(H * 6.0 - 2.0), 0, 1);
   const B = clamp(2.0 - Math.abs(H * 6.0 - 4.0), 0, 1);
@@ -23,7 +23,7 @@ export function HUEToRGB(H) {
 }
 
 export function HSVToRGB(H, S, V) {
-  const [R, G, B] = HUEToRGB(H);
+  const [R, G, B] = HueToRGB(H);
   return [
     ((R - 1.0) * S + 1.0) * V,
     ((G - 1.0) * S + 1.0) * V,
@@ -34,14 +34,12 @@ export function HSVToRGB(H, S, V) {
 export function RGBToHSV(R, G, B) {
   const Cmin = Math.min(R, Math.min(G, B));
   const Cmax = Math.max(R, Math.max(G, B));
-  // All components are the same so the color is just grayscale
+  // If all components are the same then the color is just grayscale
   if (Cmin === Cmax) return [0, 0, Cmin];
-  // Assume 360 grad = 1 hue
-  const angle = 60 / 360;
   const delta = Cmax - Cmin;
   let hue;
-  if (Cmax === R) hue = angle * (G - B / delta) % 6;
-  else if (Cmax === G) hue = angle * (B - R / delta) + 2;
-  else if (Cmax === B) hue = angle * (R - G / delta) + 4;
-  return [hue, delta / Cmax, Cmax];
+  if (Cmax === R) hue = 60 * (G - B) / delta;
+  else if (Cmax === G) hue = 60 * (B - R) / delta + 120;
+  else if (Cmax === B) hue = 60 * (R - G) / delta + 240;
+  return [hue / 360, delta / Cmax, Cmax];
 }
