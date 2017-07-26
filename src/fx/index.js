31664f3b93b457cbef79a83a5ef0eae155e1621b
Danila Shutov <dcbrwn2@gmail.com>
2017-07-26 04:26:46 +0400

    Add contrast nad brightness shader

--- src/fx/index.js
+++ src/fx/index.js
@@ -4,14 +4,16 @@ import grayscale from './grayscale.js';
 import scaline from './scaline.js';
 import holgaart from './holgaart.js';
 import motionBlur from './motionBlur.js';
+import contrast from './contrast.js';
 import tint from './tint.js';
 
 export default {
+  contrast,
   colorize,
   funky,
   grayscale,
-  scaline,
   holgaart,
-  tint,
   motionBlur,
+  scaline,
+  tint,
 };
