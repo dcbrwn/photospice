2bd861eb1a18b79d181324b6913dd28ef478ae38
Danila Shutov <dcbrwn2@gmail.com>
2017-08-01 16:10:25 +0400

    Convert remaining effects to new format

--- src/fx/index.js
+++ src/fx/index.js
@@ -1,4 +1,6 @@
 const req = require.context('./', false, /^.*\.yml$/);
-const effects = req.keys().map(req);
+const effects = req.keys()
+  .map(req)
+  .sort((a, b) => a.name.localeCompare(b.name));
 
 export default effects;
